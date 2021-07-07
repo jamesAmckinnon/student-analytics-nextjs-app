c8387828b5a25b1d4fb265a1258d801405058382
jamesAmckinnon <james.mckinnon95@gmail.com>
2021-07-07 00:24:25 -0600

    f

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -7,28 +7,6 @@ export default function Nav({ title = 'Student Analytics' }) {
   const [session, loading] = useSession();
   const [count, setCount] = useState("");
 
-  var axios = require("axios").default;
-
-  var options2 = {
-    method: 'POST',
-    url: 'https://personal-portal.us.auth0.com/oauth/token',
-    headers: {'content-type': 'application/x-www-form-urlencoded'},
-    data: {
-      grant_type: 'client_credentials',
-      client_id: 'WRZQNo7nfg58nReIUeEIU4OfZVBRh37C',
-      client_secret: 'VIRgfllEuFVxQPlwaO4PHZGHj1Oe2jFqF4QCrsQtFY-y4urZRaFxrO8TyHpFj2PC',
-      audience: 'https://personal-portal.us.auth0.com/api/v2/'
-    }
-  };
-
-  axios.request(options2).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-
-
-
   if(session){
     var axios = require("axios").default;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,27 @@ function RedirectPage({ ctx }) {
   }
 }
 
+
+var axios = require("axios").default;
+
+var options2 = {
+  method: 'POST',
+  url: 'https://personal-portal.us.auth0.com/oauth/token',
+  headers: {'content-type': 'application/x-www-form-urlencoded'},
+  data: {
+    grant_type: 'client_credentials',
+    client_id: 'WRZQNo7nfg58nReIUeEIU4OfZVBRh37C',
+    client_secret: 'VIRgfllEuFVxQPlwaO4PHZGHj1Oe2jFqF4QCrsQtFY-y4urZRaFxrO8TyHpFj2PC',
+    audience: 'https://personal-portal.us.auth0.com/api/v2/'
+  }
+};
+
+axios.request(options2).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
 RedirectPage.getInitialProps = ctx => {
   // We check for ctx.res to make sure we're on the server.
   if (ctx.res) {
