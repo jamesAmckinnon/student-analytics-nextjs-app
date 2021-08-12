6d8f535acebeb71b3a8e6ee2b2699f0c37b68e08
jamesAmckinnon <james.mckinnon95@gmail.com>
2021-08-11 21:22:37 -0600

    k

--- components/logic/index.tsx
+++ components/logic/index.tsx
@@ -1,19 +1,31 @@
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useState, useEffect } from 'react'
 
 export default function Logic ({children}) {
     const [session, loading] = useSession();
+    const [width, setWidth] = useState(window.innerWidth);
+
+    
+    useEffect(() => {
+        function handleResize() {
+        setWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [width]);
 
     if(!loading) {
         return (
             <>     
-                {session && (
+
+                {session && (width > 870) && (
                     <>
                         <div className="overflow-y-hidden h-full">
                             {children}
                         </div>
                     </>
-                )} 
-                {!session && (
+                )}
+                {!session && (width > 870) && (
                     <div className="w-full h-full">          
                         <div className="signInWrapper">
                             <div className="signInTile">
@@ -25,6 +37,27 @@ export default function Logic ({children}) {
                         </div>
                     </div>
                 )}
+
+                {session && (width < 870) && (
+                    <>
+                        <div className="h-full">
+                            {children}
+                        </div>
+                    </>
+                )}
+                {!session && (width < 870) && (
+                    <div className="w-full h-full">          
+                        <div className="signInWrapper">
+                            <div className="signInTile">
+                                <div className="signIn">
+                                    <h3 className="">Not Signed In</h3>
+                                    <button className="font-bold " onClick={() => signIn( 'auth0', { callbackUrl: 'http://localhost:3000/dashboard/home' } )}>Sign in</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            
             </>
         )
     } else {
