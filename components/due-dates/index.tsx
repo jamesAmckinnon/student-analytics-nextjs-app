5794d2df6d7b88ff386a03f4525026aa6a436d17
jamesAmckinnon <james.mckinnon95@gmail.com>
2021-08-27 17:52:56 -0600

    J

--- components/due-dates/index.tsx
+++ components/due-dates/index.tsx
@@ -202,7 +202,7 @@ function DueDates( {user_id, current_semester} ) {
 
   return (
       <>    
-        <div className="flex flex-row justify-center">  
+        <div className="flex flex-row justify-center pb-12">  
           {due_dates && orderDueDates(due_dates, daysUntil(due_dates))}
         </div>
       </>
