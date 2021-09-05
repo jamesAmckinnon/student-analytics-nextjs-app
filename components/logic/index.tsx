125c8a324788d2b0a4d91a2e78c2120449afbedb
jamesAmckinnon <james.mckinnon95@gmail.com>
2021-09-04 23:43:35 -0600

    F

--- components/logic/index.tsx
+++ components/logic/index.tsx
@@ -28,7 +28,7 @@ export default function Logic ({children}) {
                                     <div className="flex flex-col items-center justify-center">
                                         <h3 className="text-3xl font-bold">Student Dashboard</h3>
                                         <div className="signIn mt-12">
-                                            <button className="font-bold border border-black px-2 py-2px rounded-sm" onClick={() => signIn( 'auth0', { callbackUrl: 'http://localhost:3000/dashboard/home' } )}>Log In {'>'}</button>
+                                            <button className="font-bold border border-black px-2 py-2px rounded-sm" onClick={() => signIn( 'auth0', { callbackUrl: 'http://localhost:3000/dashboard/home' } )}>Log In / Sign up {' >'}</button>
                                         </div>
                                     </div>
                                 </div>
