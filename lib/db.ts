f2b35535d03544b56f9ce54ed793d5f168e29d84
jamesAmckinnon <--unset>
2022-05-13 20:12:07 -0600

    First Commit

--- lib/db.ts
+++ lib/db.ts
@@ -10,10 +10,8 @@ export const db = mysql({
   },
 })
 
-export async function query(
-  q: string,
-  values: (string | number)[] | string | number = []
-) {
+export async function query( q: string, values: (string | number)[] | string | number = [] ) {
+
   try {
     const results = await db.query(q, values)
     await db.end()
@@ -21,4 +19,5 @@ export async function query(
   } catch (e) {
     throw Error(e.message)
   }
+  
 }
